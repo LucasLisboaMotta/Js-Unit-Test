66d3f5378506b478cba491b1b068ebd5edfe40d4
Lucs Lisboa <lucas@pop-os.localdomain>
2021-12-08 17:21:33 -0300

    requisito 5 (corrigindo linha 18)

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (estudante) => {
+function createStudent(estudante) {
   return {
     name: estudante,
     feedback: () => 'Eita pessoa boa!',
   };
-};
+}
 
 module.exports = createStudent;
